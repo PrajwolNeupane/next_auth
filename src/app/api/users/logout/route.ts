2b2d5f4c9855c84bc2fb8e2b5cc9c43b89831131
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-07-20 09:11:24 +0545

    LogOut Action Added

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try{
+
+        const response = NextResponse.json({
+            message:'Logout Successful',
+            success:true
+        });
+        response.cookies.set("token","",{httpOnly:true,expires:new Date(0)})
+        return response;
+    }catch(e:any){
+        return NextResponse.json({error:e.message},{status:500})
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,25 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 export default function ProfilePage(){
+
+    const router = useRouter();
+
+    const logOut = async () => {
+        try{
+            const response = await axios.get("/api/users/logout");
+            router.push("/login");
+        }catch(e:any){
+            console.log(e.message);
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <button onClick={logOut} className="p-2 mt-2 bg-gray-800 text-gray-100 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Log Out</button>
         </div>  
     )
 }
